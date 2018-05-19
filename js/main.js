c6e89f0b4879c90bc81c3fe2204b1d34020e293d
Tom Sampson <tbsam@TOM-HPENVY.rs>
2018-05-18 19:55:42 -0500

    more functions

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
 
 // Global variables
 var tubbieStatus = 0; // 0 = default starting, 1 = available for battle, 2 = ready to attack, 3 = defeated
-var opponentStatus = 0;
-var playerEnergy = 0;
-var opponentEnergy = 0;
-var playerMinAtk = 0;
-var playerMaxAtk = 0;
-// Teletubbies Stats
-var allTubbies = [
+var opponentStatus = false; // false if no opponent, true if an opponent has been selected
+
+// All of the tubbies
+const allTubbies = [
     {
         id: 0,
         sname: "tinky_winky",
@@ -50,7 +47,12 @@ var allTubbies = [
     }
 ];
 
-
+// Arrays used to store tubbies
+var chosenTubbie = new Array(); // tubbie player has chosen to play as
+var opponentTubbie = new Array(); // this tubbie is the active opponent
+var availableTubbies = new Array(); // remaining tubbies
+var availableTubbies = [].concat(allTubbies) // Innitially all tubbies are available
+var defeatedTubbies = new Array(); // defeated tubbies
 
 // List the Teletubbies in the about section
 console.log("We have " + allTubbies.length + " Teletubbies ready to play!");
@@ -58,8 +60,8 @@ for (var i = 0; i < allTubbies.length; i++) {
     $( ".list-unstyled" ).append( $( `<li><a href="${allTubbies[i].wiki}" target="_blank" class="text-white">${allTubbies[i].name}</a></li>` ) );
 }
 
-// The arena - all tubbies are in the arena at the start of game, after that only battling tubbies will appear in the arena
-function makeCards(tubbieList, putWhere, tubbieStatus) {
+// Place the tubbies in the appropriate place 
+function placeCards(tubbieList, putWhere, tubbieStatus) {
     var buttonLabel = '';
     var hideButton = false;
     var buttonColor = 'btn-outline-secondary';
@@ -97,20 +99,57 @@ function makeCards(tubbieList, putWhere, tubbieStatus) {
                 </div>
             </div>
         </div>`));
-            
+
         if (hideButton===false) { // Only show button when it's time to pick a tubbie
             $( `.d-flex.justify-content-between.align-items-center.${tubbieList[i].sname}` ).append( $( `<div class="btn-group"><button type="button" class="btn ${buttonColor} btn-sm ${tubbieList[i].id}">${buttonLabel}</button></div>`));
         }
-        
-
-        
-        
     }
 }
 
+// add a tubbie to an array
+function addTubbie(tubbieType){
+    switch(tubbieType) {
+        case "chosen":
+
+            break;
+        case "'opponent":
+
+            break;
+        case "available":
+
+            break;
+        case "defeated":
+
+            break;   
+    }
+}
+
+// remove a tubbie from an array
+function removeTubbie(tubbieType){
+    switch(tubbieType) {
+        case "chosen":
+
+            break;
+        case "'opponent":
+
+            break;
+        case "available":
+
+            break;
+        case "defeated":
+
+            break;   
+    }
+}
+
+
+
+
+
+
 // START GAME
 function startGame(startingLineup){
-    makeCards(startingLineup, "arena", 0); // Show all the tubbies in the arena at start of game and set game status 0 (new game)
+    placeCards(startingLineup, "arena", 0); // Show all the tubbies in the arena at start of game and set game status 0 (new game)
 }
 startGame(allTubbies);
 
@@ -118,7 +157,31 @@ function resetGame () {
     $( ".row.arena" ).empty(); 
 }
 
+function chooseOponent(availableTubbies, opponentTubbieId) {
+        //create a new array for the opponent tubbie
+        for(var i = 0; i < availableTubbies.length; i++) {
+            var checkOpponentId = parseInt(availableTubbies[i].id); // find matching id in available tubbies
+            if (checkOpponentId === opponentTubbieId) {
+                console.log("found opponent tubbie");
+                var opponentTubbie = [].concat(availableTubbies[i]); // add tubbie with matching id to opponent array
+                $(".row.chatter").empty();
+                $(".row.chatter").append(`<p>You've chosen to battle ${opponentTubbie[0].name}!  Now press the attack!</p>`); // chosen tubbie id is always 0
+                opponentStatus = true; //set opponent status to 1 (an opponent has been chosen)
 
+                
+                for(var i = 0; i < availableTubbies.length; i++){
+                    if (parseInt(availableTubbies[i].id) === opponentTubbieId) {
+                        console.log("Found matching opponent id: " + availableTubbies[i].id)
+                        availableTubbies.splice(i,1); //remove the opponent tubbie from list of available tubbies
+                    }
+            }
+            placeCards(opponentTubbie, "arena", 3); //put the other tubbies in the dugout
+            $( ".row.dugout" ).empty();
+            placeCards(availableTubbies, "dugout", 3); //put the other tubbies in the dugout
+            return opponentTubbie;    
+        }            
+    }
+}
 
 
 // Select a Teletubbie to play
@@ -128,7 +191,6 @@ $(".btn").click(function() {
     console.log("Player chose tubbie id: " + chosenTubbieId);
     // clear the arena
     $( ".row.arena" ).empty();
-
 
     //remove chosen tubbie from available tubbies
     var availableTubbies = [].concat(allTubbies); //Make a copy of the full list of tubbies
@@ -148,16 +210,18 @@ $(".btn").click(function() {
     console.log(chosenTubbie);
     $(".row.chatter").empty();   
     $(".row.chatter").append(`<p>You chose ${chosenTubbie[0].name}! Now choose your first opponent!</p>`); // chosen tubbie id is always 0
-    
-    makeCards(chosenTubbie, "arena", 2); //put the other tubbies in the dugout
-    makeCards(availableTubbies, "dugout", 1); //put the other tubbies in the dugout
+    placeCards(chosenTubbie, "arena", 2); //put the other tubbies in the dugout
+    placeCards(availableTubbies, "dugout", 1); //put the other tubbies in the dugout
 
     // Check to see if attack button was pressed
     $(".btn.btn-danger").click(function() {
         console.log("Attack Button Pressed");
-        if (opponentStatus===0) {
+        if (opponentStatus===false) {
             $(".row.chatter").empty();
             $(".row.chatter").append(`<p>You must first have an opponent to attack!  Choose one of the Teletubbies below</p>`); // chosen tubbie id is always 0
+        } else if (opponentStatus===true){
+            $(".row.chatter").empty();
+            $(".row.chatter").append(`<p>You attack ${opponentTubbie[0].name}!</p>`); // attack message
         }
     });
 
@@ -167,31 +231,13 @@ $(".btn").click(function() {
         var opponentTubbieId = parseInt(opponentClasses[3]);
         console.log("Player chose opponent tubbie id: " + opponentTubbieId);
         console.log("Battle Button Pressed");
-        if (opponentStatus===0) { // If no opponent chosen yet
-
-                //create a new array for the opponent tubbie
-                for(var i = 0; i < availableTubbies.length; i++) {
-                    var checkOpponentId = parseInt(availableTubbies[i].id); // find matching id in available tubbies
-                    if (checkOpponentId === opponentTubbieId) {
-                        console.log("found opponent tubbie");
-                        var opponentTubbie = [].concat(availableTubbies[i]); // add tubbie with matching id to opponent array
-                        $(".row.chatter").empty();
-                        $(".row.chatter").append(`<p>You've chosen to battle ${opponentTubbie[0].name}!  Now press the attack!</p>`); // chosen tubbie id is always 0
-                        
-                        for(var i = 0; i < availableTubbies.length; i++){
-                            if (parseInt(availableTubbies[i].id) === opponentTubbieId) {
-                                console.log("Found matching opponent id: " + availableTubbies[i].id)
-                                availableTubbies.splice(i,1); //remove the opponent tubbie from list of available tubbies
-                            }
-                    }
-                    makeCards(opponentTubbie, "arena", 3); //put the other tubbies in the dugout
-                    $( ".row.dugout" ).empty();
-                    makeCards(availableTubbies, "dugout", 3); //put the other tubbies in the dugout
-                }            
-            }
+        if (opponentStatus===false) { // If no opponent chosen yet
+            chooseOponent(availableTubbies, opponentTubbieId);
         }
-    });    
+    });       
  });
+
+ 
 
  
  
